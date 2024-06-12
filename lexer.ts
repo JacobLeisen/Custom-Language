0e2e69e77182e891650e8f818ffe0365ecc1f3e9
Jacob Leisen <jmleisen@icloud.com>
2024-06-12 18:43:34 -0500

    Added functions to identify letter or int

--- lexer.ts
+++ lexer.ts
@@ -20,6 +20,16 @@ function token (value = "", type: TokenType): Token {
     return {value, type}
 }
 
+function isalpha (src: string) {
+    return src.toUpperCase() != src.toLowerCase();
+}
+
+function isint (str: string) {
+    const c = str.charCodeAt(0)
+    const bounds = ['0'.charCodeAt(0), '9'.charCodeAt(0)];
+    return (c >= bounds[0] && c <= bounds[1]);
+}
+
 export function tokenize (sourceCode: string): Token[] {
     const tokens = new Array<Token>();
     // Note if making series lexer don't split as it bleeds a lot of memory
@@ -33,7 +43,15 @@ export function tokenize (sourceCode: string): Token[] {
             tokens.push(token(src.shift(), TokenType.CloseParen))
         } else if (src[0] == '+' || src[0] == '-' || src[0] == '/' || src[0] == '*') { 
             tokens.push(token(src.shift(), TokenType.BinaryOperator))
-        } 
+        } else if (src[0] == '=') { 
+            tokens.push(token(src.shift(), TokenType.Equals))
+        } else {
+            // Handle multicharacter tokens
+
+        }
+
+
+
     }
 
     return tokens;
